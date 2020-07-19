5c3a1cd3d7aab4ae44fa72e5af117dc87fc6224f
杰克有茶 <chenwansal1@163.com>
2020-07-19 20:11:26 +0800

    修改: 文件目录 新增: App.js -> 服务端， 默认调用7000接口

---
+++ App.js
@@ -0,0 +1,27 @@
+// 引用express
+var express = require("express");
+var bodyParser = require("body-parser");
+var multer = require("multer");
+var upload = multer();
+var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true}));
+
+app.use(express.static(require('path').join(__dirname, 'views/public')));
+
+app.set("views", __dirname);
+
+app.set( 'view engine', 'html' );
+app.engine( '.html', require( 'ejs' ).__express);
+
+// 监听首页
+app.get("/", function(req, res) {
+    res.render("views/index.html")
+});
+
+// 监听端口
+let port = 7000;
+app.listen(port, () => {
+    console.log("正在监听" + port);
+})
